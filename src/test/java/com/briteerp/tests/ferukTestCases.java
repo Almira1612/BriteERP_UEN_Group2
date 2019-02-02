19648212a238183cbb418c478be1fe6f64ba05db
SeizeThed4y <frk.coskunn@gmail.com>
2019-02-01 19:22:54 -0500

    feruktestcase update

--- src/test/java/com/briteerp/tests/ferukTestCases.java
+++ src/test/java/com/briteerp/tests/ferukTestCases.java
@@ -1,10 +1,7 @@
 package com.briteerp.tests;
 
 
-import com.briteerp.pages.EnvironmentPage;
-import com.briteerp.pages.HomePage;
-import com.briteerp.pages.POSHomePage;
-import com.briteerp.pages.LoginPage;
+import com.briteerp.pages.*;
 import com.briteerp.utilities.Driver;
 import com.briteerp.utilities.TestBase;
 import org.openqa.selenium.support.PageFactory;
@@ -14,6 +11,7 @@ import org.testng.annotations.Test;
 
 public class ferukTestCases extends TestBase {
     ferukTestCases() {
+
         PageFactory.initElements(Driver.getDriver(), LoginPage.class);
     }
 
@@ -24,19 +22,30 @@ public class ferukTestCases extends TestBase {
         new EnvironmentPage();
         new POSHomePage();
         new HomePage();
+        new POSReportingOrders();
     }
 
 
     @Test(groups = "ST")
-    public void test01() throws InterruptedException {
+    public void test01() {
 
+        extentLogger = report.createTest("Verify the report`s page");
+        extentLogger.info("Selecting the environment");
         EnvironmentPage.selectEnvironment();
+        extentLogger.info("Verifying the title contain Login");
         Assert.assertTrue(LoginPage.actualTitle.contains("Login"));
+        extentLogger.info("Entering valid user credentials and click login");
         LoginPage.login();
-        Thread.sleep(5000);
+        extentLogger.info("Verifying the title contains Inbox");
         Assert.assertTrue(HomePage.currentTitle.contains("Inbox"));
-        Thread.sleep(5000);
+        extentLogger.info("Clicking the PointofSale module");
         HomePage.pointOfSaleElement.click();
+        extentLogger.info("Verifying the title contains Point of Sale");
+        Assert.assertTrue(POSHomePage.currentTitle.contains("Point of Sale"));
+        extentLogger.info("Clicking the orders under reports");
+        POSHomePage.reportingOrdersElement.click();
+        extentLogger.info("Verifying the title contains Analysis");
+        Assert.assertTrue(POSReportingOrders.currentTitle.contains("Analysis"));
 
 
     }
